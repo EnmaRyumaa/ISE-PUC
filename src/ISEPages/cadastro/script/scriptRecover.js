2eb09aa2fc6b2a5ef64d5683fb360a60422fb627
Fernando Auguste <fer.augustofs@gmail.com>
2023-06-22 21:44:23 -0300

    alteracao script para api

--- src/ISEPages/cadastro/script/scriptRecover.js
+++ src/ISEPages/cadastro/script/scriptRecover.js
@@ -3,84 +3,102 @@ const recoverInputLastName = document.getElementById('lastname');
 const recoverInputEmail = document.getElementById('email');
 const recoverInputPassword = document.getElementById('password');
 const recoverButtonCreate = document.getElementById('bt-create');
+const recoverSelectUserType = document.getElementById('usertype');
+
+const urlJSON = "https://api-json-server-tiaw.vercel.app/user";
 
 const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 recoverButtonCreate.addEventListener('click', async () => {
-    await includeNewUser();
+  await includeNewUser();
 });
 
-async function insertValuesInObject(recoverInputName, recoverInputLastName, recoverInputEmail, recoverInputPassword) {
-    let newUser = {
-        name: recoverInputName.value,
-        lastName: recoverInputLastName.value,
-        email: recoverInputEmail.value,
-        password: recoverInputPassword.value
-    };
-
-    return newUser;
-}
-
 async function includeNewUser() {
-    let newStrUser = { user: [] };
-    let objUser = await insertValuesInObject(
-        recoverInputName,
-        recoverInputLastName,
-        recoverInputEmail,
-        recoverInputPassword
-    );
+  const objUser = await insertValuesInObject(
+    recoverInputName,
+    recoverInputLastName,
+    recoverInputEmail,
+    recoverInputPassword,
+    recoverSelectUserType.value
+  );
 
-    newStrUser.user.push(objUser);
+  if (validateIfFieldEmpty(objUser)) {
+    alert("Por favor, preencha todos os campos.");
+    return;
+  }
 
-    if (validateIfFieldEmpty(objUser)) {
-        alert("Por favor, preencha todos os campos.");
-        return;
+  if (!validateEmail(objUser.email)) {
+    alert("Por favor, insira um e-mail válido.");
+    return;
+  }
+
+  const userExists = await validateUserInDB(objUser);
+  if (userExists) {
+    alert("Email e/ou senha já estão cadastrados.");
+    return;
+  }
+
+  try {
+    const response = await fetch(urlJSON, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(objUser)
+    });
+
+    if (response.ok) {
+      alert('Usuário cadastrado com sucesso!');
+    } else {
+      throw new Error('Erro ao cadastrar usuário.');
     }
-
-    if (!validateEmail(objUser.email)) {
-        alert("Por favor, insira um e-mail válido.");
-        return;
-    }
-
-    if (validateUserInDB(objUser)) {
-        alert("Email e/ou senha já estão cadastrados.");
-        return;
-    }
-
-    saveObjectInDB(newStrUser);
+  } catch (error) {
+    console.error('Erro:', error);
+    alert('Erro ao cadastrar usuário. Por favor, tente novamente.');
+  }
 }
 
 function validateIfFieldEmpty(user) {
-    for (let key in user) {
-        if (user.hasOwnProperty(key) && user[key] === '') {
-            return true;
-        }
+  for (let key in user) {
+    if (user.hasOwnProperty(key) && user[key] === '') {
+      return true;
     }
-    return false;
+  }
+  return false;
 }
 
 function validateEmail(email) {
-    return emailRegex.test(email);
+  return emailRegex.test(email);
 }
 
-function validateUserInDB(newUser) {
-    let existingData = localStorage.getItem('userData');
-    if (existingData) {
-        let existingUserArray = JSON.parse(existingData).user;
+async function validateUserInDB(newUser) {
+  try {
+    const response = await fetch(urlJSON);
+    const users = await response.json();
 
-        return existingUserArray.some(user => user.email === newUser.email || user.password === newUser.password);
-    }
+    return users.some(
+      user => user.email === newUser.email || user.password === newUser.password
+    );
+  } catch (error) {
+    console.error('Erro:', error);
     return false;
+  }
 }
 
-function saveObjectInDB(newStrUser) {
-    let existingData = localStorage.getItem('userData');
-    let existingUserArray = existingData ? JSON.parse(existingData).user : [];
+async function insertValuesInObject(
+  recoverInputName,
+  recoverInputLastName,
+  recoverInputEmail,
+  recoverInputPassword,
+  userType
+) {
+  let newUser = {
+    name: recoverInputName.value,
+    lastName: recoverInputLastName.value,
+    email: recoverInputEmail.value,
+    password: recoverInputPassword.value,
+    type: userType
+  };
 
-    existingUserArray.push(...newStrUser.user);
-
-    let updatedData = { user: existingUserArray };
-    localStorage.setItem('userData', JSON.stringify(updatedData));
-
-    alert('Usuário cadastrado com sucesso!');
+  return newUser;
 }
