09d03d679ea09af44151a6c13236407261fbe55c
thetabelini <felipetabelinipena@gmail.com>
2023-06-22 22:51:51 -0300

    Primeira expedição da tela de login integrada com o login

---
+++ src/ISEPages/login/scriptLogin.js
@@ -0,0 +1,33 @@
+function validate() {
+    let recoverEmail = document.getElementById('email').value;
+    let recoverPassword = document.getElementById('password').value;
+  
+    fetch('https://api-json-server-tiaw.vercel.app/user')
+      .then(res => res.json())
+      .then(users => {
+        const escolheUser = users.find(user => user.email === recoverEmail && user.password === recoverPassword);
+        if (escolheUser) {
+          switch (escolheUser.type) {
+            case 'professor':
+              window.location.href = '../professor/telaProfessores.html';
+              break;
+            case 'responsavel':
+              window.location.href = '../responsavel/telaResponsavel.html';
+              break;
+            case 'coordenacao':
+              window.location.href = '../coordenacao/telaCoordenacao.html';
+              break;
+            default:
+              alert('Tipo de usuário inválido');
+          }
+        } else {
+          alert('Usuário ou senha incorretos.');
+        }
+      })
+      .catch(error => {
+        console.error('Erro na requisição:', error);
+      });
+  }
+
+ 
+  
